8a2baef86f6f75e7913d2e3cacf25e88ddb78e10
Raman Walwyn-Venugopal <ramanvenu94@gmail.com>
2015-04-08 23:30:47 -0500

    bst lab

--- bst/src/bst/core.clj
+++ bst/src/bst/core.clj
@@ -23,17 +23,26 @@
 
 (defn size "Return the size of the tree."
   [t]
-  0)
+  (:size t))
 
 ;; # Add
 ;;
 ;; The nodes will be entered into the tree on the basis of their key.
 ;; If someone tries to add a key that is already there, we replace the value
 ;; with the new entry.
+(declare find-helper)
+(declare find)
+
+(defn add-helper [node nu-key nu-val]
+  (cond (nil? node) (make-node nu-key nu-val)
+        (= nu-key (:key node)) (make-node (:left node) (:key node) nu-val (:right node))
+        (pos? (compare nu-key (:key node))) (make-node (:left node) (:key node) (:value node) (add-helper (:right node) nu-key nu-val))
+        :else (make-node (add-helper (:left node) nu-key nu-val) (:key node) (:value node) (:right node))))
+
 
 (defn add "Add a key and value to the BST."
   [bst nu-key nu-val]
-  nil)
+  (if (nil? (:root bst)) (BST. (make-node nu-key nu-val) 1) (BST. (add-helper (:root bst) nu-key nu-val) (if (nil? (find bst nu-key)) (+ 1 (:size bst)) (:size bst)))))
 
 ;; # Find
 ;;
@@ -42,24 +51,56 @@
 ;; version of the function must search the entire tree!  If the search item is not
 ;; there, return nil.
 
+(defn find-helper [node look-key]
+  (cond (nil? node) nil
+        (= look-key (:key node)) (:value node)
+        (pos? (compare look-key (:key node))) (find-helper (:right node) look-key)
+  :else (find-helper (:left node) look-key)))
+
 (defn find "Look for a key and return the corresponding value."
-  [bst look-key] nil)
+  [bst look-key] (find-helper (:root bst) look-key))
+
+
+(defn find-key-helper [node look-value]
+  (cond (nil? node) nil
+        (= look-value (:value node)) (:key node)
+        :else (or (find-key-helper (:right node) look-value) (find-key-helper (:left node) look-value))))
+
 
 (defn find-key "Look for a value and return the corresponding key."
-  [bst look-value] nil)
+  [bst look-value] (find-key-helper (:root bst) look-value))
 
 ;; # Delete
 ;;
 ;; Similiarly, we have two versions of delete.  Please use the predecessor node if
 ;; you need to delete a child with two elements.
 
+
+
+(defn get-pred [node]
+  (if (nil? (:right node)) node (get-pred (:right node))))
+
+
+
+(defn delete-helper [node victim]
+  (cond (nil? node) (make-tree)
+        (= victim (:key node)) (cond (and (nil? (:left node)) (nil? (:right node))) nil
+                                     (or (nil? (:left node)) (nil? (:right node))) (or (:left node) (:left node))
+                                     :else (let [pred (get-pred (:left node))]
+                                             (make-node (delete-helper (:left node) (:key pred))(:key pred) (:value pred) (:right node)))) 
+        (pos? (compare victim (:key node))) (make-node (:left node) (:key node) (:value node) (delete-helper (:right node) victim))
+        :else (make-node (delete-helper (:left node) victim) (:key node) (:value node) (:right node))))
+
+
+
 (defn delete [bst victim]
-  nil
-  )
+  (if (nil? (find bst victim)) bst (BST. (delete-helper (:root bst) victim) (-> bst :size dec))))
 
 (defn delete-value [bst victim]
-  nil
-  )
+  (let [fkey (find-key bst victim)] 
+    (if (nil? fkey) bst (delete bst fkey))))
+
+
 
 ;; # Map Tree
 ;;
@@ -67,5 +108,11 @@
 ;; If your tree is ((x 3 x) 5 ((x 7 x) 6 x)), then (map-tree t inc)
 ;; will return ((x 4 x) 6 ((x 8 x) 7 x))
 
+(defn map-tree-helper [node  f] 
+  (if (nil? node) nil (make-node (map-tree-helper (:left node) f) (:key node) (f (:value node)) (map-tree-helper (:right node) f))))
+
 (defn map-tree
-  [t f] nil)
+  [t f] (BST. (map-tree-helper (:root t) f) (:size t)))
+
+
+
--- bst/test/bst/t_core.clj
+++ bst/test/bst/t_core.clj
@@ -4,5 +4,78 @@
   (:import [bst.core BST] ))
 
 (facts "about this lab"
-  (fact "the student never started it."
-        (+ 1 2)  => 12))
+  
+
+  (facts "about make-tree"
+    (fact "makes an empty tree"
+      (:root (make-tree)) => nil)
+    (fact "size is zero"
+      (:size (make-tree)) => 0))
+  
+
+  (facts "about make-node"
+    ( fact "right and left are nil when given key and value"
+      (and (:right (make-node 10 20)) (:left (make-node 10 20))) => true))
+
+  (def tree (add (add (add (add (add (add (make-tree) 10 30) 20 40) 5 15) 30 60) 1 13) 25 13))
+
+  (facts "about size" 
+    (fact "returns the correct size of the binary tree"
+      (:size (make-tree)) => 0
+      (:size tree) => 6))
+
+  (facts "about add"
+    (fact "increments size when adding a new key to the tree"
+      (:size (add tree 3 7)) => 7)
+    (fact "does not increment when adding with existing key"
+      (:size (add tree 20 15)) => 6)
+    (fact "puts value in the correct place"
+      (= tree (add (delete tree 1) 1 13)) => true))
+  
+  (facts "about find"
+    (fact "returns nil if key does not exist"
+      (find tree 45) => nil)
+    (fact "returns correct value"
+      (find tree 25) => 13))
+
+  (facts "about find-key"
+    (fact "returns nil when value does not exist"
+      (find-key tree 45) => nil)
+    (fact "returns correct key"
+      (find-key tree 15) => 5)
+    (fact "returns right most key when finding multiple"
+      (find-key tree 13) => 25))
+
+  (facts "about delete"
+    (fact "returns bst tree when key does not exist"
+      (= tree (delete tree 100)) => true)
+    (fact "deletes key and decrements size"
+      (:size (delete tree 5)) => 5)
+    (fact "replaces correct predecessor when deleting child with two children"
+      (:key (:root (delete tree 10))) => 5)
+    (fact "replaces only child when deleting tree with only one child"
+      (:key (:root   (delete (add (add (make-tree) 10 20) 5 15) 10))) => 5
+      (:key (:root   (delete (add (add (make-tree) 10 20) 15 15) 10))) => 15)
+    (fact "removes predecessor after deleting"
+      (:key (:left (:root (delete tree 10)))) => 1))
+
+  (facts "about delete-value"
+    (fact "returns bst tree when value does not exist"
+      (= tree (delete-value tree 100)) => true)
+    (fact "deletes key and decrements size"
+      (:size (delete-value tree 15)) => 5)
+    (fact "replaces correct predecessor when deleting child with two children"
+      (:key (:root (delete-value tree 30))) => 5)
+    (fact "replaces only child when deleting tree with only one child"
+      (:key (:root   (delete-value (add (add (make-tree) 10 20) 5 15) 20))) => 5
+      (:key (:root   (delete-value (add (add (make-tree) 10 20) 15 15) 20))) => 15) 
+    (fact "removes predecessor after deleting"
+      (:key (:left (:root (delete-value tree 30)))) => 1)
+    (fact "deletes right most key when multiple values exist"
+      (:key (:left (:left (:root (delete-value mofo 13))))) => 1))
+  
+  (facts "about map-tree"
+    (fact "applies function to all values in tree"
+      (:value (:root (map-tree tree inc))) => 31
+      (= (map-tree tree inc) (add (add (add (add (add (add (make-tree) 10 31) 20 41) 5 16) 30 61) 1 14) 25 14)) => true)))
+  
