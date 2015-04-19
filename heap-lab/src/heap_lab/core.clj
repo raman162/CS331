3cd13eb3633d65cc90aca713dd3cf762145f04ff
Raman Walwyn-Venugopal <ramanvenu94@gmail.com>
2015-04-18 23:07:27 -0500

    heap lab

--- heap-lab/src/heap_lab/core.clj
+++ heap-lab/src/heap_lab/core.clj
@@ -47,34 +47,112 @@ this is part of the implementation, not for public consumption."
   [loc]
   (inc (* loc 2)))
 
+(defn heap-get-left [heap loc]
+  "gets the left "
+  (heap-get heap (heap-left loc)))
+
 (defn heap-right
   "Return the right index."
   [loc]
   (+ 2 (* loc 2)))
+
+(defn heap-get-right [heap loc]
+  "gets the right"
+  (heap-get heap (heap-right loc)))
 
 (defn heap-parent
   "Return the parent index."
   [loc]
   (int (/ (dec loc) 2)))
 
+(defn heap-get-parent [heap loc]
+  "gets the parent"
+  (heap-get heap (heap-parent loc)))
+
 ;; Now it's time for your code!  You need these three, but you are welcome to
 ;; write helper functions if you want (e.g., `percolate-down`.)  Do **not** write
 ;; `midje` tests for them, because they are not part of the spec.
 
+
+
 (defn top
   "Return the top element of a heap.
 If the heap has no elements, return `nil`."
-  [heap])
+  [heap]
+  (heap-get heap 0)
+  )
+
+(defn percolate [heap loc]
+ (if (< (dec (:size heap)) (or (heap-left loc) (heap-right loc))) heap  
+
+ (let [x (compare (heap-get-left heap loc) (heap-get-right heap loc))]
+    (cond (= x -1) 
+          (if (nil? (heap-get-left heap loc))
+            heap 
+            (let [y (compare (heap-get heap loc) (heap-get-left heap loc))]
+              (cond (= y -1) heap
+                    (= y 0) heap
+                    :else (let [temp (heap-get heap loc)]
+                            (percolate   (heap-set (heap-set heap loc (heap-get-left heap loc)) (heap-left loc) temp) (heap-left loc))))))
+          (= x 0)
+          (if (nil? (heap-get-left heap loc))
+            heap
+             (let [y (compare (heap-get heap loc) (heap-get-left heap loc))]
+              (cond (= y -1) heap
+                    (= y 0) heap
+                    :else (let [temp (heap-get heap loc)]
+                            (percolate   (heap-set (heap-set heap loc (heap-get-left heap loc)) (heap-left loc) temp) (heap-left loc))))))
+          :else (if (nil? (heap-get-right heap loc))
+                  heap
+                   (let [y (compare (heap-get heap loc) (heap-get-right heap loc))]
+              (cond (= y -1) heap
+                    (= y 0) heap
+                    :else (let [temp (heap-get heap loc)]
+                            (percolate   (heap-set (heap-set heap loc (heap-get-right heap loc)) (heap-right loc) temp) (heap-right loc))))))))))
+
+(defn heap-full [heap]
+  (if (nil? (heap-get heap (dec (:size heap)))) false true))
+
+
+
+(defn heap-last [heap loc]
+  "returns the next empty location"
+  (if (heap-full heap) nil 
+  (if (nil? (heap-get heap loc)) loc (heap-last heap (inc loc)))))
+
+(defn heap-last-elt [heap loc]
+  "returns the last element"
+  (if (heap-full heap) (:size heap)
+      (if (nil? (heap-get heap loc))  loc (heap-last-elt heap (inc loc)))))
 
 (defn delete
   "Deletes the first element of the heap.
 Returns the new heap."
   [heap]
-  nil)
+  (if (nil? (heap-get heap 0)) 
+    heap
+    (let [last (dec (heap-last-elt heap 0))]
+      (percolate (heap-set (heap-set heap 0 (heap-get heap last)) last nil) 0))))
+
+
+
+(defn heap-double [heap doubled-heap loc]
+  (if (nil? doubled-heap)
+    (let [doubled-heap (make-heap (* 2 (:size heap)))]
+      (heap-double heap doubled-heap loc)) 
+    (if (== loc (:size heap)) doubled-heap (heap-double heap (heap-set doubled-heap loc (heap-get heap loc)) (inc loc)))))
+
+(defn add-helper [heap loc] 
+ (if (== (compare (heap-get heap loc) (heap-get-parent heap loc)) -1) 
+   (let [temp (heap-get heap loc)]
+     (add-helper (heap-set (heap-set heap loc (heap-get-parent heap loc)) (heap-parent loc) temp) (heap-parent loc)))
+   heap))
 
 (defn add
-  "Adds a new element to the heap.
+  "Adds a new element to the heap
 If the data vector is too small, we resize it."
   [heap data]
-  nil)
+  (if (heap-full heap) (add (heap-double heap nil 0) data)
+      (add-helper (heap-set heap (heap-last heap 0) data) (heap-last heap 0)))
+  )
 
--- heap-lab/test/heap_lab/t_core.clj
+++ heap-lab/test/heap_lab/t_core.clj
@@ -7,3 +7,37 @@
              (+ 1 1)  =>  2)
        (fact "two plus one is three."
              (+ 2 1)  =>  3))
+
+
+
+
+(let [heap (add (add (add (add (add (make-heap 5) 4) 1) 2) 7) 3)]
+
+ 
+(facts "about top"
+  (fact "it returns nil when the heap has no elements"
+    (top (make-heap 10)) => nil)
+  (fact "it returns the top most element" 
+    (top heap) => 1)
+)
+
+
+(facts "about delete"
+
+  (fact "returns same heap  when trying to delete from empty heap"
+    (delete (make-heap 10)) => (make-heap 10))
+  (fact "deletes an element and replaces the correct element"
+    (top (delete heap)) => 2)
+  (fact "does not duplicate the last element"
+    (heap-get (delete heap) 4) => nil)
+  (fact "percolates down correctly"
+    (heap-get (delete heap) 3) => 7))
+
+(facts "about add"
+
+  (fact "doubles the heap if the heap is full"
+    (:size (add heap 13)) => 10)
+  (fact "larger element added doesn't move"
+    (heap-get (add heap 13) 5) => 13)
+  (fact "smallest element goes to the top"
+    (heap-get (add heap 0) 0) => 0)))
